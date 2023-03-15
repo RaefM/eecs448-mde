4ced415eb4924d15065e1602edc13987d88bc710
RaefM <maroofr@umich.edu>
2023-03-14 20:37:27 -0400

    qol improvements and bug fixes for doc2vec.py

--- doc2vec/doc2vec.py
+++ doc2vec/doc2vec.py
@@ -5,14 +5,17 @@ import pandas as pd
 from preprocess import *
 from sklearn.model_selection import train_test_split
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
+from tqdm import tqdm
 
-def train_model(data_training, vec_size=300, num_threads=6):
+def train_model(data_training, vec_size=300):
     model = Doc2Vec(
         vector_size=vec_size, 
-        workers=num_threads,
-        min_count=2, 
+        workers=-1,
+        min_count=3, 
         epochs=30, 
-        seed=448
+        seed=448,
+        hs=1,
+        negative=0
     )
     model.build_vocab(data_training)
     model.train(data_training, total_examples=model.corpus_count, epochs=model.epochs)
@@ -30,7 +33,7 @@ def construct_d2v_df(model, vecs, X, vec_size=300):
 
 def X_to_tagged_set(X):
     def to_tagged_doc(post): return TaggedDocument(word_tokenize(post['processed_body']), post['id'])
-    return X_train.apply(to_tagged_doc, axis=1)
+    return X.apply(to_tagged_doc, axis=1)
 
 def get_data():
     base_df = pd.read_csv('aita_preprocessed_new.csv', sep='\t')
@@ -42,14 +45,15 @@ def get_data():
     
     return X, data_training, data_testing
 
-def construct_d2v_of_size_v(v=300, X, data_training, data_testing):
-    print("Training a doc2vec model with vectors of size " + str(v))
+def construct_d2v_of_size_v(X, data_training, data_testing, v=300):
+    print("Training a doc2vec model with vectors of size v = " + str(v) + "...")
     model = train_model(data_training, vec_size=150)
     vecs = infer_all_vecs(model, X)
     
+    print("Saving results...")
     doc2vec_df, word_to_embedding = construct_d2v_df(model, vecs, X)
     doc2vec_df.to_csv('aita_doc2vec_' + str(v) + '.csv', sep='\t', encoding='utf-8')
-    model.save('aita_doc2vec_' + + str(v) + 'model')
+    model.save('aita_doc2vec_' + + str(v) + '_model')
     
     training_score = model.score(data_testing, total_sentences=len(data_testing))
     print("Doc2Vec model with v = " + str(v) + " had a log prob of " + str(training_score))
@@ -58,8 +62,8 @@ def construct_d2v_of_size_v(v=300, X, data_training, data_testing):
 if __name__ == "__main__":
     np.random.seed(448)
     X, data_training, data_testing = get_data()
-    for v in [100, 150, 200, 300, 500, 750, 1000]:
-        construct_d2v_of_size_v(v, data_training, data_testing)
+    for v in tqdm([100, 150, 200, 300, 500, 750, 1000]):
+        construct_d2v_of_size_v(X, data_training, data_testing, v)
         
     pass
     
